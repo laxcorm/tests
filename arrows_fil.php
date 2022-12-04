f94370d0de9b2d6964f684ade5afbba0d62cd66b
laxcorm <2468abc000@gmail.com>
2022-12-04 23:09:58 +0200

    delete done

--- arrows_fil.php
+++ arrows_fil.php
@@ -1,10 +1,7 @@
-
-
 <?php 
 $back = $id - 1;
 $forward = $id +1;
-?>
-<?php if ($id != 1) : ?>
+if ($id != 1) : ?>
 <a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'].'?id='.$back ?>" role="button"><</a>
 <?php endif; ?>
 <a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'].'?id='.$forward ?>" role="button">></a>
--- delete.php
+++ delete.php
@@ -1,1 +1,8 @@
 <?php
+require('mysql.php');
+$id = $_GET['id'];
+$query = 'DELETE FROM tests WHERE id=:id';
+$stmt = $db -> prepare($query);
+$stmt -> execute([':id' => $id]);
+
+header("Location: http://".$_SERVER['HTTP_HOST']."/fillin.php");
