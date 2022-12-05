2c1a63e85bbbc1ee732a3679b5d58e24bc809697
laxcorm <2468abc000@gmail.com>
2022-12-05 22:51:31 +0200

    count.php reworked - check of session[count] added

--- count.php
+++ count.php
@@ -8,12 +8,16 @@
 }
     $count = $_SESSION['count'] ?? 0; */
 
+if (!isset($_SESSION['count'])) {
     $query = "SELECT COUNT(*) FROM tests";
     $stmt = $db->prepare($query);
     $stmt->execute();
     $count = (int)$stmt->fetch(PDO::FETCH_ASSOC)['COUNT(*)'];
-
-
+    $_SESSION['count'] = $count;
+}
+else{
+    $count = $_SESSION['count'];
+}
 
 
 /* if ( $_SESSION['count'] || $_SERVER['REQUEST_METHOD'] == 'POST') {
