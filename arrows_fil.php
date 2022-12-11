c2f79e7ac2e6a323771bfc27a4050dfeb344d4d1
laxcorm <2468abc000@gmail.com>
2022-12-11 17:41:47 +0200

    arrows fill modified forward if id != count

--- arrows_fil.php
+++ arrows_fil.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
 $back = $id - 1;
-$forward = $id +1;
+$forward = $id + 1;
 if ($id != 1) : ?>
-<a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'].'?id='.$back ?>" role="button"><</a>
+    <a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'] . '?id=' . $back ?>" role="button"></a>
 <?php endif; ?>
-<a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'].'?id='.$forward ?>" role="button">></a>
+<?php if ($id != $count) : ?>
+    <a class="btn btn-primary" href="<?php echo $_SERVER['PHP_SELF'] . '?id=' . $forward ?>" role="button">></a>
+<?php endif; ?>
